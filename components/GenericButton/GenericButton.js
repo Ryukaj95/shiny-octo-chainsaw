f3e0da0a97c26743d5c3c807c86087331aa448af
Francesco Cambi <fcambi@develer.com>
2018-07-12 17:37:16 +0200

    Init GenericButton component

---
+++ components/GenericButton/GenericButton.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { StyleSheet, View, Text, TouchableHighlight } from 'react-native'
+
+export default class GenericButton extends React.Component {
+
+};
+
+const styles = StyleSheet.create({
+
+})
