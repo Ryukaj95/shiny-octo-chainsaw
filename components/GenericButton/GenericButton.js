490cdec5062b6045587329f72ce28d8510c636ab
Francesco Cambi <fcambi@develer.com>
2018-07-12 18:55:19 +0200

    Add getStylesheet method in GenericButton

--- components/GenericButton/GenericButton.js
+++ components/GenericButton/GenericButton.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import { View, Text, TouchableHighlight } from 'react-native'
+import { StyleSheet, View, Text, TouchableHighlight } from 'react-native'
 import PropTypes from 'prop-types'
+import _ from 'lodash'
 
 export default class GenericButton extends React.Component {
+  constructor () {
+    super()
+    this.getStylesheet = this.getStylesheet.bind(this)
+  }
+
+  getStylesheet (style) {
+    if (_.isNil(style)) return {}
+    if (_.isInteger(style)) return style
+    if (_.isObject(style)) return StyleSheet.create({style}).style
+  }
+
   render () {
     return (
       <TouchableHighlight onPress={() => this.props.onGenericPress()}>
-        <View style={this.props.containerLayout || {}}>
-          <Text style={this.props.textLayout || {}}>
+        <View style={this.getStylesheet(this.props.containerLayout)}>
+          <Text style={this.getStylesheet(this.props.textLayout)}>
             {this.props.text}
           </Text>
         </View>
@@ -18,7 +30,7 @@ export default class GenericButton extends React.Component {
 
 GenericButton.propTypes = {
   text: PropTypes.string.isRequired,
-  containerLayout: PropTypes.object,
-  textLayout: PropTypes.object,
+  containerLayout: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
+  textLayout: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
   onGenericPress: PropTypes.func.isRequired
 }
