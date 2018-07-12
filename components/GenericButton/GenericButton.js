78484102b19cb71c1f57a175475213c5cc355edb
Francesco Cambi <fcambi@develer.com>
2018-07-12 17:50:21 +0200

    Add basic button functionality to GenericButton comp

--- components/GenericButton/GenericButton.js
+++ components/GenericButton/GenericButton.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react'
-import { StyleSheet, View, Text, TouchableHighlight } from 'react-native'
+import React from 'react'
+import { View, Text, TouchableHighlight } from 'react-native'
+import PropTypes from 'prop-types'
 
 export default class GenericButton extends React.Component {
-
+  render () {
+    return (
+      <TouchableHighlight>
+        <View style={this.state.containerLayout || {}}>
+          <Text style={this.state.textLayout || {}}>
+            {this.state.buttonText}
+          </Text>
+        </View>
+      </TouchableHighlight>
+    )
+  }
 };
 
-const styles = StyleSheet.create({
-
-})
+GenericButton.propTypes = {
+  buttonText: PropTypes.string.isRequired,
+  containerLayout: PropTypes.object,
+  textLayout: PropTypes.object,
+  onPress: PropTypes.func
+}
